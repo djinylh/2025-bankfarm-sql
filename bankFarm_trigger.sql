f854d234fb2d04769d34853574b8e668ef324a1c
djinylh <d.jinylh@gmail.com>
2025-12-02 18:23:39 +0900

    [feat] 대출 가입시 [대출 상환]테이블, [공통 계약] 테이블 데이터 입력 트리거 제작

--- bankFarm_trigger.sql
+++ bankFarm_trigger.sql
@@ -34,3 +34,74 @@ WHERE acct_id = NEW.acct_id;
 
 END $$
 DELIMITER ;
+
+
+/*																													대출 가입 트리거 																	*/
+
+DROP TRIGGER IF EXISTS trg_insert_loan
+
+DELIMITER $$
+
+CREATE TRIGGER trg_insert_loan
+AFTER INSERT ON loan
+FOR EACH ROW
+BEGIN
+
+DECLARE i INT DEFAULT 1; -- 반복문용
+DECLARE totalMonth INT; -- 대출 기간
+DECLARE firstMonth DATE; -- 대출 시작일
+DECLARE totalAmt BIGINT; -- 대출 총 금액
+DECLARE oneMonthAmt BIGINT; -- 상환 원 금액
+DECLARE rate DECIMAL(6,4); -- 실제 이자
+DECLARE totalMonthAmt BIGINT; -- 한달 실제 금액
+DECLARE app_id BIGINT;
+DECLARE bran_id BIGINT;
+
+SELECT 
+	la.loan_req_trm
+	, la.loan_dcsn_dt 
+	, la.loan_req_amt 
+	, l.loan_fn_intrst
+	, la.loan_app_id
+	, b.bran_id
+INTO totalMonth, firstMonth, totalAmt, rate, app_id, bran_id
+FROM loan l
+LEFT JOIN loan_application la
+ON l.loan_app_id = la.loan_app_id
+LEFT JOIN employees e
+ON la.emp_id = e.emp_id
+LEFT JOIN branch b
+ON b.bran_id = e.bran_id
+WHERE la.loan_app_id = NEW.loan_app_id;
+
+SET oneMonthAmt = ROUND(totalAmt / totalMonth,0); -- 상환 원 금액
+SET totalMonthAmt = ROUND(oneMonthAmt*rate,0); -- 한달 실제 금액
+
+-- 반복문 시작 [ 대출 기간만큼 row 만들어줄 것 ]
+-- repayment 필요 데이터
+-- 대출 ID, 매달 상환금, 전체 이자, 이자가 곱해진 금액, 한달씩 늘어나기, 기본 스테이터스값
+WHILE i <= totalMonth DO
+INSERT INTO loan_repayment (loan_id, loan_rpmt_prncp, loan_rpmt_intrst, loan_rpmt_due, loan_due_dt, loan_due_sts)
+VALUE (NEW.loan_id, oneMonthAmt, rate, totalMonthAmt, DATE_ADD(firstMonth, INTERVAL i MONTH) ,'TX003');
+
+SET i = i + 1;
+
+END WHILE;
+
+
+INSERT INTO prod_document (
+	bran_id
+	, doc_prod_tp
+	, doc_prod_id
+	, doc_nm
+)
+VALUE (
+	bran_id
+	, 'PD007'
+	, app_id
+	, '대출 계약 문서 이름'
+);
+
+END $$
+
+DELIMITER ;
