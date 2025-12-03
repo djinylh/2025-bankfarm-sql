d3bb536cf4c0c369824535817c4f34277fd6c10a
djinylh <d.jinylh@gmail.com>
2025-12-03 18:16:50 +0900

    [feat] 고객 가입 프로시저 제작

--- bankFarm_procedure.sql
+++ bankFarm_procedure.sql
@@ -1,53 +1,101 @@
 
-/*																							대출 연체 프로시저 																	*/
+/*																															대출 연체 프로시저 																	*/
 DROP PROCEDURE IF EXISTS proc_loan_overdue;
 DELIMITER $$
 
 CREATE PROCEDURE proc_loan_overdue()
 BEGIN
 -- 공통 조인
-CREATE TEMPORARY TABLE temp_rs AS 
-    SELECT pr.prod_id, SUM(rr.rt_pct) AS total_rt_pct
-    FROM prod_rate pr
-    JOIN rate_rule rr ON rr.rt_id = pr.prod_rt_id AND rr.rt_tp = 'RT002'
-    WHERE pr.prod_tp = 'RT007'
-      AND pr.prod_id IN (
-          SELECT loan_id 
-          FROM loan_repayment 
-          WHERE loan_due_sts = 'TX007'
-      )
-    GROUP BY pr.prod_id;
+	CREATE TEMPORARY TABLE temp_rs AS 
+	    SELECT pr.prod_id, SUM(rr.rt_pct) AS total_rt_pct
+	    FROM prod_rate pr
+	    JOIN rate_rule rr ON rr.rt_id = pr.prod_rt_id AND rr.rt_tp = 'RT002'
+	    WHERE pr.prod_tp = 'RT007'
+	      AND pr.prod_id IN (
+	          SELECT loan_id 
+	          FROM loan_repayment 
+	          WHERE loan_due_sts = 'TX007'
+	      )
+	    GROUP BY pr.prod_id;
 
 --									대출상환 아이디, 대출 아이디, 대출 스테이터스, 대출 상환금, 연체 시작일, 연체 이자, 연체 총 금액
-INSERT INTO loan_overdue(loan_rpmt_id, loan_id, loan_od_due_sts, loan_od_amt, loan_od_st_dt, loan_od_intrst, loan_od_fn_amt)
-SELECT
-  lr.loan_rpmt_id, -- 대출 상환 아이디
-  lr.loan_id, -- 대출 아이디
-  'TX007',
-    lr.loan_rpmt_due, -- 대출 금액
-  lr.loan_due_dt, -- 대출 일시
-  lr.loan_rpmt_intrst + COALESCE(rs.total_rt_pct, 0), -- 대출 총 이자 ( 원래 이자 + 가산 금리 )
-  ROUND(lr.loan_rpmt_due * (lr.loan_rpmt_intrst + COALESCE(rs.total_rt_pct,0)),0)
-FROM loan_repayment lr
-LEFT JOIN temp_rs
-    ON temp_rs.prod_id = lr.loan_id
-WHERE lr.loan_due_sts = 'TX007'
-AND lr.loan_due_dt = CURDATE() - INTERVAL 1 DAY;
+		INSERT INTO loan_overdue(loan_rpmt_id, loan_id, loan_od_due_sts, loan_od_amt, loan_od_st_dt, loan_od_intrst, loan_od_fn_amt)
+		SELECT
+		  lr.loan_rpmt_id, -- 대출 상환 아이디
+		  lr.loan_id, -- 대출 아이디
+		  'TX007',
+		    lr.loan_rpmt_due, -- 대출 금액
+		  lr.loan_due_dt, -- 대출 일시
+		  lr.loan_rpmt_intrst + COALESCE(rs.total_rt_pct, 0), -- 대출 총 이자 ( 원래 이자 + 가산 금리 )
+		  ROUND(lr.loan_rpmt_due * (lr.loan_rpmt_intrst + COALESCE(rs.total_rt_pct,0)),0)
+		FROM loan_repayment lr
+		LEFT JOIN temp_rs
+		    ON temp_rs.prod_id = lr.loan_id
+		WHERE lr.loan_due_sts = 'TX007'
+		AND lr.loan_due_dt = CURDATE() - INTERVAL 1 DAY;
 
 -- 공통 연체에도 이동
-INSERT INTO overdue_history(od_id, cust_id, od_source_id, od_tp, od_st_dt, od_amt)
-SELECT UUID(), la.cust_id, lr.loan_rpmt_id, 'PD007', lr.loan_due_dt, ROUND(lr.loan_rpmt_due * (lr.loan_rpmt_intrst + COALESCE(rs.total_rt_pct,0)),0)
-FROM loan_repayment lr
-LEFT JOIN temp_rs
-    ON temp_rs.prod_id = lr.loan_id
-    LEFT JOIN loan_application la
-    ON lr.loan_id = la.loan_id
-WHERE lr.loan_due_sts = 'TX007'
-AND lr.loan_due_dt = CURDATE() - INTERVAL 1 DAY;
+		INSERT INTO overdue_history(od_id, cust_id, od_source_id, od_tp, od_st_dt, od_amt)
+		SELECT UUID(), la.cust_id, lr.loan_rpmt_id, 'PD007', lr.loan_due_dt, ROUND(lr.loan_rpmt_due * (lr.loan_rpmt_intrst + COALESCE(rs.total_rt_pct,0)),0)
+		FROM loan_repayment lr
+		LEFT JOIN temp_rs
+		    ON temp_rs.prod_id = lr.loan_id
+		    LEFT JOIN loan_application la
+		    ON lr.loan_id = la.loan_id
+		WHERE lr.loan_due_sts = 'TX007'
+		AND lr.loan_due_dt = CURDATE() - INTERVAL 1 DAY;
 
 
 END $$
 DELIMITER ;
 
+/* 																															고객 가입 프로시저 																*/
 
+DROP PROCEDURE IF EXISTS proc_customer_join;
+DELIMITER $$
 
+CREATE PROCEDURE proc_customer_join(
+	IN p_cust_nm VARCHAR(11)
+	,IN p_cust_phone VARCHAR(13)
+	,IN p_cust_email VARCHAR(30)
+	,IN p_cust_birth VARCHAR(10)
+	,IN p_cust_crd_point INT
+	,IN p_cust_ssn VARCHAR(20)
+	,IN p_cust_cd VARCHAR(5)
+	,IN p_cust_tp VARCHAR(5)
+	,IN p_cust_marketing_yn CHAR(1)
+	,IN p_cust_business_number VARCHAR(14)
+	,IN p_cust_company_name VARCHAR(20)
+	,IN p_cust_fax VARCHAR(30)
+)
+BEGIN
+
+	DECLARE new_cust_id BIGINT; -- 변수 저장
+	
+	INSERT INTO customer
+	(cust_nm, cust_phone, cust_email ,  cust_birth, cust_crd_point, cust_ssn, cust_cd, cust_tp, cust_marketing_yn)
+	VALUE
+	(p_cust_nm, p_cust_phone, p_cust_email, p_cust_birth, p_cust_crd_point, p_cust_ssn, p_cust_cd, p_cust_tp, p_cust_marketing_yn);
+	
+	 SET new_cust_id = LAST_INSERT_ID(); -- 위의 customer의 PK 생성시 여기에 담아줌
+	 
+	 IF p_cust_tp <> 'CU011' -- 일반 고객이 아니라면
+	 THEN
+	 	IF  	p_cust_business_number IS NULL
+	 		OR p_cust_company_name IS NULL
+	 		OR p_cust_fax IS NULL
+		THEN
+	 		SIGNAL SQLSTATE '45000'
+			SET MESSAGE_TEXT = '사업자번호, 회사이름, 팩스번호는 필수로 입력해야 합니다.';
+	 		
+	 ELSE
+	 	INSERT INTO cust_business_corporation
+	 	(cust_business_number, cust_id, cust_company_name, cust_fax)
+	 	VALUE
+	 	(p_cust_business_number,new_cust_id, p_cust_company_name, p_cust_fax);
+	 	END IF;
+	 		
+	 END IF;		
+	 
+END $$
+DELIMITER ;
--- bankFarm_trigger.sql
+++ bankFarm_trigger.sql
@@ -7,35 +7,35 @@ BEFORE INSERT ON `transaction`
 FOR EACH ROW
 BEGIN
 
-DECLARE amount INT; -- 계좌 잔액 변수 설정
-DECLARE fee INT; -- 수수료
+	DECLARE amount INT; -- 계좌 잔액 변수 설정
+	DECLARE fee INT; -- 수수료
 
-SELECT trns_fee INTO fee
-FROM trns_fee
-WHERE trns_fee_id = NEW.trns_fee_id;
+	SELECT trns_fee INTO fee
+	FROM trns_fee
+	WHERE trns_fee_id = NEW.trns_fee_id;
 
-SELECT acct_bal INTO amount -- acct_bal 의 보유 잔액을 amount에 넣겠다
-FROM `account`
-WHERE acct_id = NEW.acct_id; -- NEW.acct_id는 인서트 됐을 때 넘어오는 acct_id 이거랑 원래 acct_id랑 연결하는 것
+	SELECT acct_bal INTO amount -- acct_bal 의 보유 잔액을 amount에 넣겠다
+	FROM `account`
+	WHERE acct_id = NEW.acct_id; -- NEW.acct_id는 인서트 됐을 때 넘어오는 acct_id 이거랑 원래 acct_id랑 연결하는 것
 
-IF NEW.trns_amt < 0 THEN -- trns_amt가 출금인지 ( - 10000 ) 입금인지 ( 10000 ) 확인하는 IF문
+	IF NEW.trns_amt < 0 THEN -- trns_amt가 출금인지 ( - 10000 ) 입금인지 ( 10000 ) 확인하는 IF문
+		
+		/* 여기서 다른 IF문 시작 */
+		IF ABS(NEW.trns_amt) > amount THEN
+			SIGNAL SQLSTATE '45000' -- 보통 강제 오류발생은 45000을 날린다고 함. 
+			SET MESSAGE_TEXT = '잔액이 부족합니다';
+		END IF;
+		/*	다른 IF문 끝 */
+		SET NEW.trns_bal = amount + NEW.trns_amt - fee; -- 출금 반영
 
-/* 여기서 다른 IF문 시작 */
-IF ABS(NEW.trns_amt) > amount THEN
-SIGNAL SQLSTATE '45000' -- 보통 강제 오류발생은 45000을 날린다고 함. 
-SET MESSAGE_TEXT = '잔액이 부족합니다';
-END IF;
-/*	다른 IF문 끝 */
-SET NEW.trns_bal = amount + NEW.trns_amt - fee; -- 출금 반영
+		/* 출금이 아니라면 .... */
+	ELSE -- 여기에서 위의 큰 IF문을 끝내는 END IF; 와 같은 효력을 가짐
+		SET NEW.trns_bal = amount + NEW.trns_amt - fee; -- 입금 반영
+	END IF;
 
-/* 출금이 아니라면 .... */
-ELSE -- 여기에서 위의 큰 IF문을 끝내는 END IF; 와 같은 효력을 가짐
-SET NEW.trns_bal = amount + NEW.trns_amt - fee; -- 입금 반영
-END IF;
-
-UPDATE `account` -- 원래 계좌의 보유 잔액으로 업데이트하기
-SET acct_bal = NEW.trns_bal
-WHERE acct_id = NEW.acct_id;
+	UPDATE `account` -- 원래 계좌의 보유 잔액으로 업데이트하기
+	SET acct_bal = NEW.trns_bal
+	WHERE acct_id = NEW.acct_id;
 
 END $$
 DELIMITER ;
@@ -52,66 +52,53 @@ AFTER INSERT ON loan
 FOR EACH ROW
 BEGIN
 
-DECLARE i INT DEFAULT 1; -- 반복문용
-DECLARE totalMonth INT; -- 대출 기간
-DECLARE firstMonth DATE; -- 대출 시작일
-DECLARE totalAmt BIGINT; -- 대출 총 금액
-DECLARE oneMonthAmt BIGINT; -- 상환 원 금액
-DECLARE rate DECIMAL(6,4); -- 실제 이자
-DECLARE totalMonthAmt BIGINT; -- 한달 실제 금액
-DECLARE app_id BIGINT;
-DECLARE bran_id BIGINT;
+	DECLARE i INT DEFAULT 1; -- 반복문용
+	DECLARE totalMonth INT; -- 대출 기간
+	DECLARE firstMonth DATE; -- 대출 시작일
+	DECLARE totalAmt BIGINT; -- 대출 총 금액
+	DECLARE oneMonthAmt BIGINT; -- 상환 원 금액
+	DECLARE rate DECIMAL(6,4); -- 실제 이자
+	DECLARE totalMonthAmt BIGINT; -- 한달 실제 금액
+	DECLARE app_id BIGINT;
+	DECLARE bran_id BIGINT;
 
-SELECT 
-	la.loan_req_trm
-	, la.loan_dcsn_dt 
-	, la.loan_req_amt 
-	, l.loan_fn_intrst
-	, la.loan_app_id
-	, b.bran_id
-INTO totalMonth, firstMonth, totalAmt, rate, app_id, bran_id
-FROM loan l
-LEFT JOIN loan_application la
-ON l.loan_app_id = la.loan_app_id
-LEFT JOIN employees e
-ON la.emp_id = e.emp_id
-LEFT JOIN branch b
-ON b.bran_id = e.bran_id
-WHERE la.loan_app_id = NEW.loan_app_id;
+	SELECT 
+		la.loan_req_trm
+		, la.loan_dcsn_dt 
+		, la.loan_req_amt 
+		, l.loan_fn_intrst
+		, la.loan_app_id
+		, b.bran_id
+	INTO totalMonth, firstMonth, totalAmt, rate, app_id, bran_id
+	FROM loan l
+	LEFT JOIN loan_application la
+	ON l.loan_app_id = la.loan_app_id
+	LEFT JOIN employees e
+	ON la.emp_id = e.emp_id
+	LEFT JOIN branch b
+	ON b.bran_id = e.bran_id
+	WHERE la.loan_app_id = NEW.loan_app_id;
 
-SET oneMonthAmt = ROUND(totalAmt / totalMonth,0); -- 상환 원 금액
-SET totalMonthAmt = ROUND(oneMonthAmt*rate,0); -- 한달 실제 금액
+	SET oneMonthAmt = ROUND(totalAmt / totalMonth,0); -- 상환 원 금액
+	SET totalMonthAmt = ROUND(oneMonthAmt*rate,0); -- 한달 실제 금액
 
--- 반복문 시작 [ 대출 기간만큼 row 만들어줄 것 ]
--- repayment 필요 데이터
--- 대출 ID, 매달 상환금, 전체 이자, 이자가 곱해진 금액, 한달씩 늘어나기, 기본 스테이터스값
-WHILE i <= totalMonth DO
-INSERT INTO loan_repayment (loan_id, loan_rpmt_prncp, loan_rpmt_intrst, loan_rpmt_due, loan_due_dt, loan_due_sts)
-VALUE (NEW.loan_id, oneMonthAmt, rate, totalMonthAmt, DATE_ADD(firstMonth, INTERVAL i MONTH) ,'TX003');
+	-- 반복문 시작 [ 대출 기간만큼 row 만들어줄 것 ]
+	-- repayment 필요 데이터
+	-- 대출 ID, 매달 상환금, 전체 이자, 이자가 곱해진 금액, 한달씩 늘어나기, 기본 스테이터스값
+	WHILE i <= totalMonth DO
+		INSERT INTO loan_repayment (loan_id, loan_rpmt_prncp, loan_rpmt_intrst, loan_rpmt_due, loan_due_dt, loan_due_sts)
+		VALUE (NEW.loan_id, oneMonthAmt, rate, totalMonthAmt, DATE_ADD(firstMonth, INTERVAL i MONTH) ,'TX003');
+	
+		SET i = i + 1;
 
-SET i = i + 1;
-
-END WHILE;
+	END WHILE;
 
 
-INSERT INTO prod_document (
-	bran_id
-	, doc_prod_tp
-	, doc_prod_id
-	, doc_nm
-)
-VALUE (
-	bran_id
-	, 'PD007'
-	, app_id
-	, '대출 계약 문서 이름'
-);
+	INSERT INTO prod_document 
+	(	bran_id, doc_prod_tp, doc_prod_id, doc_nm	)
+	VALUE 
+	(	bran_id, 'PD007', app_id, '대출 계약 문서 이름' );
 
 END $$
 
 DELIMITER ;
-
-/*																									계좌 트리거													*/
-
-
-
